ac05ab0c5d054ad90a315758af162f4ce464c0f3
Zanan Pech <zanan@student.ubc.ca>
2025-01-07 20:21:53 -0800

    Remove exclude pattern from config file

--- docs/conf.py
+++ docs/conf.py
@@ -23,14 +23,4 @@ extensions = [
 ]
 autoapi_dirs = ["../src"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "sphinx_rtd_theme"
